1124aa59d5a00ddb16be8c863894cb65d1b41961
ra100 <ra100x@gmail.com>
2016-01-10 17:02:25 +0100

    wait till wifi connects

--- init.lua
+++ init.lua
@@ -7,5 +7,11 @@ node.compile('server.lua')
 node.compile('dht.lua')
 node.compile('watcher.lua')
 
-dofile('watcher.lc')
-dofile('server.lc')
+tmr.alarm(1,1000, 0, function()
+  if wifi.sta.status() == 5 then
+    tmr.stop(0)
+    print(wifi.sta.getip())
+    dofile('watcher.lc')
+    dofile('server.lc')
+  end
+end)
