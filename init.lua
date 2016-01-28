672471820d069493a061fd41efa64f00d4a89bc3
ra100 <ra100x@gmail.com>
2016-01-28 18:38:39 +0100

    wifi monitor

--- init.lua
+++ init.lua
@@ -1,16 +1,18 @@
-dofile('wifi.lua')
-
 files = file.list()
 
 if (files['server.lc'] == nil) then node.compile('server.lua') end
 if (files['dht.lc'] == nil) then node.compile('dht.lua') end
 if (files['watcher.lc'] == nil) then node.compile('watcher.lua') end
 
-tmr.alarm(1,1000, 0, function()
-  if wifi.sta.status() == 5 then
-    tmr.stop(0)
-    print(wifi.sta.getip())
-    dofile('watcher.lc')
-    dofile('server.lc')
-  end
+wifi.sta.eventMonReg(wifi.STA_GOTIP, function()
+  wifi.sta.eventMonStop("unreg all")
+  print(wifi.sta.getip())
+  dofile('watcher.lc')
+  dofile('server.lc')
 end)
+
+wifi.sta.eventMonStart()
+
+dofile('wifi.lua')
+
+
